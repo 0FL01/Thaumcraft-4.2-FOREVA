4592fdd192b8897e256d628fbb4bf1f33b786cb0
Andrey Bash <andrej89536995626@gmail.com>
2026-05-17 05:11:55 +0300

    feat(stage6): restore primal arrow launch and spawn contracts
        Changes:\n    - Add EntityPrimalArrow reference launch constructors for coordinate spawn and typed velocity launch\n    - Expand primal arrow spawn payload to include motion, rotation, arrow type, and shooter entity id reconstruction\n    - Add EntityPrimalArrowCoreContractsStaticGuardTest and update GOAL_PROGRESS

--- src/main/java/thaumcraft/common/entities/projectile/EntityPrimalArrow.java
+++ src/main/java/thaumcraft/common/entities/projectile/EntityPrimalArrow.java
@@ -1,6 +1,7 @@
 package thaumcraft.common.entities.projectile;
 
 import io.netty.buffer.ByteBuf;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.IProjectile;
 import net.minecraft.entity.projectile.EntityArrow;
@@ -13,17 +14,43 @@ import net.minecraft.network.datasync.EntityDataManager;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.EntityDamageSourceIndirect;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.registry.IEntityAdditionalSpawnData;
 
 public class EntityPrimalArrow extends EntityArrow implements IProjectile, IEntityAdditionalSpawnData {
     private static final DataParameter<Integer> ARROW_TYPE =
         EntityDataManager.createKey(EntityPrimalArrow.class, DataSerializers.VARINT);
+    private int shootingEntityId = -1;
 
     public EntityPrimalArrow(World world) { super(world); }
     public EntityPrimalArrow(World world, EntityLivingBase shooter) {
         super(world, shooter);
+        this.shootingEntityId = shooter.getEntityId();
+    }
+    public EntityPrimalArrow(World world, double x, double y, double z) {
+        super(world);
+        this.setSize(0.25F, 0.25F);
+        this.setPosition(x, y, z);
+    }
+    public EntityPrimalArrow(World world, EntityLivingBase shooter, float velocity, int type) {
+        super(world);
+        this.shootingEntity = shooter;
+        this.shootingEntityId = shooter.getEntityId();
+        this.setArrowType(type);
+        this.setSize(0.5F, 0.5F);
+        this.setLocationAndAngles(shooter.posX, shooter.posY + (double) shooter.getEyeHeight(), shooter.posZ, shooter.rotationYaw, shooter.rotationPitch);
+        this.posX -= (double) (MathHelper.cos(this.rotationYaw * 0.017453292F) * 0.16F);
+        this.posY -= 0.10000000014901161D;
+        this.posZ -= (double) (MathHelper.sin(this.rotationYaw * 0.017453292F) * 0.16F);
+        Vec3d look = shooter.getLookVec();
+        this.posX += look.x;
+        this.posY += look.y;
+        this.posZ += look.z;
+        this.setPosition(this.posX, this.posY, this.posZ);
+        this.shoot(shooter, shooter.rotationPitch, shooter.rotationYaw, 0.0F, velocity * 1.5F, 1.0F);
     }
 
     @Override
@@ -88,6 +115,34 @@ public class EntityPrimalArrow extends EntityArrow implements IProjectile, IEnti
         this.setArrowType(nbt.getInteger("arrowType"));
     }
 
-    @Override public void writeSpawnData(ByteBuf buf) { buf.writeInt(this.getArrowType()); }
-    @Override public void readSpawnData(ByteBuf buf) { this.setArrowType(buf.readInt()); }
+    @Override
+    public void writeSpawnData(ByteBuf buf) {
+        buf.writeDouble(this.motionX);
+        buf.writeDouble(this.motionY);
+        buf.writeDouble(this.motionZ);
+        buf.writeFloat(this.rotationYaw);
+        buf.writeFloat(this.rotationPitch);
+        buf.writeByte(this.getArrowType());
+        buf.writeInt(this.shootingEntity != null ? this.shootingEntity.getEntityId() : this.shootingEntityId);
+    }
+
+    @Override
+    public void readSpawnData(ByteBuf buf) {
+        this.motionX = buf.readDouble();
+        this.motionY = buf.readDouble();
+        this.motionZ = buf.readDouble();
+        this.rotationYaw = buf.readFloat();
+        this.rotationPitch = buf.readFloat();
+        this.prevRotationYaw = this.rotationYaw;
+        this.prevRotationPitch = this.rotationPitch;
+        this.setArrowType(buf.readByte());
+        this.shootingEntityId = buf.readInt();
+        try {
+            Entity entity = this.world != null ? this.world.getEntityByID(this.shootingEntityId) : null;
+            if (entity instanceof EntityLivingBase) {
+                this.shootingEntity = (EntityLivingBase) entity;
+            }
+        } catch (Exception ignored) {
+        }
+    }
 }
