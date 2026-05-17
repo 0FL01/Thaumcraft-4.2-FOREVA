ebc8fc83c4709b361e00e99ff1577865b8a1b6d9
Andrey Bash <andrej89536995626@gmail.com>
2026-05-17 06:51:23 +0300

    fix(stage4): restore magic workbench charger vis flow
    Changes:
    
    - Port TileMagicWorkbenchCharger to reference-shaped TileVisRelay inheritance with expanded render AABB
    
    - Restore server tick wand charging from magic workbench slot 10 using vis relay consumption
    
    - Add static guard coverage and record checkpoint delta in GOAL_PROGRESS

--- src/main/java/thaumcraft/common/tiles/TileMagicWorkbenchCharger.java
+++ src/main/java/thaumcraft/common/tiles/TileMagicWorkbenchCharger.java
@@ -1,6 +1,61 @@
 package thaumcraft.common.tiles;
-import net.minecraft.util.ITickable;
-import thaumcraft.api.TileThaumcraft;
-public class TileMagicWorkbenchCharger extends TileThaumcraft implements ITickable {
-    @Override public void update() {}
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import thaumcraft.api.aspects.Aspect;
+import thaumcraft.api.aspects.AspectList;
+import thaumcraft.common.items.wands.ItemWandCasting;
+
+public class TileMagicWorkbenchCharger extends TileVisRelay {
+    public short orientation = 0;
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public AxisAlignedBB getRenderBoundingBox() {
+        return new AxisAlignedBB(
+                this.pos.getX(),
+                this.pos.getY() - 1,
+                this.pos.getZ(),
+                this.pos.getX() + 1,
+                this.pos.getY() + 1,
+                this.pos.getZ() + 1);
+    }
+
+    @Override
+    public boolean isSource() {
+        return false;
+    }
+
+    @Override
+    public void update() {
+        super.update();
+        if (this.world == null || this.world.isRemote) {
+            return;
+        }
+        TileEntity below = this.world.getTileEntity(this.pos.down());
+        if (!(below instanceof TileMagicWorkbench)) {
+            return;
+        }
+        TileMagicWorkbench workbench = (TileMagicWorkbench) below;
+        ItemStack wand = workbench.getStackInSlot(10);
+        if (wand.isEmpty() || !(wand.getItem() instanceof ItemWandCasting)) {
+            return;
+        }
+
+        ItemWandCasting wandItem = (ItemWandCasting) wand.getItem();
+        AspectList room = wandItem.getAspectsWithRoom(wand);
+        if (room == null || room.size() <= 0) {
+            return;
+        }
+        for (Aspect aspect : room.getAspects()) {
+            int drain = Math.min(5, ItemWandCasting.getMaxVis(wand) - ItemWandCasting.getVis(wand, aspect));
+            if (drain <= 0) {
+                continue;
+            }
+            ItemWandCasting.addRealVis(wand, aspect, this.consumeVis(aspect, drain), true);
+        }
+    }
 }
