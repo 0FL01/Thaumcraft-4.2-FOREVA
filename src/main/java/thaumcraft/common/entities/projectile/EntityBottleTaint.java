173ee8d5fbfbbb2fb2132408789e2d84a2ae408d
Andrey Bash <bash8de@proton.me>
2026-07-05 12:31:31 +0300

    Ignore taint bottle thrower client-side

--- src/main/java/thaumcraft/common/entities/projectile/EntityBottleTaint.java
+++ src/main/java/thaumcraft/common/entities/projectile/EntityBottleTaint.java
@@ -1,6 +1,8 @@
 package thaumcraft.common.entities.projectile;
 
+import io.netty.buffer.ByteBuf;
 import java.util.List;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.projectile.EntityThrowable;
 import net.minecraft.potion.PotionEffect;
@@ -8,6 +10,7 @@ import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.common.registry.IEntityAdditionalSpawnData;
 import thaumcraft.api.entities.ITaintedMob;
 import thaumcraft.common.Thaumcraft;
 import thaumcraft.common.config.Config;
@@ -15,11 +18,14 @@ import thaumcraft.common.config.ConfigBlocks;
 import thaumcraft.common.lib.utils.Utils;
 import thaumcraft.common.lib.world.ThaumcraftWorldGenerator;
 
-public class EntityBottleTaint extends EntityThrowable {
-    private static final int THROWER_IMPACT_GRACE_TICKS = 10;
+public class EntityBottleTaint extends EntityThrowable implements IEntityAdditionalSpawnData {
+    private int throwerId = -1;
 
     public EntityBottleTaint(World world) { super(world); }
-    public EntityBottleTaint(World world, EntityLivingBase shooter) { super(world, shooter); }
+    public EntityBottleTaint(World world, EntityLivingBase shooter) {
+        super(world, shooter);
+        this.throwerId = shooter.getEntityId();
+    }
 
     @Override
     protected float getGravityVelocity() { return 0.05f; }
@@ -27,9 +33,7 @@ public class EntityBottleTaint extends EntityThrowable {
     @Override
     protected void onImpact(RayTraceResult result) {
         if (result == null) return;
-        if (this.ticksExisted <= THROWER_IMPACT_GRACE_TICKS
-            && result.entityHit != null
-            && result.entityHit == this.getThrower()) {
+        if (this.isThrower(result.entityHit)) {
             return;
         }
         if (!this.world.isRemote) {
@@ -66,5 +70,23 @@ public class EntityBottleTaint extends EntityThrowable {
             Thaumcraft.proxy.bottleTaintBreak(this.world, this.posX, this.posY, this.posZ);
         }
         this.setDead();
+    }
+
+    private boolean isThrower(Entity entity) {
+        if (entity == null) return false;
+        EntityLivingBase thrower = this.getThrower();
+        if (entity == thrower) return true;
+        return this.throwerId >= 0 && entity.getEntityId() == this.throwerId;
+    }
+
+    @Override
+    public void writeSpawnData(ByteBuf buf) {
+        EntityLivingBase thrower = this.getThrower();
+        buf.writeInt(thrower != null ? thrower.getEntityId() : this.throwerId);
+    }
+
+    @Override
+    public void readSpawnData(ByteBuf buf) {
+        this.throwerId = buf.readInt();
     }
 }
