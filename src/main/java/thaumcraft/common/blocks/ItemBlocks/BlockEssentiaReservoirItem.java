f0a410edf0678d7a85916dd958a8142a260b1e26
Andrey Bash <bash8de@proton.me>
2026-07-05 09:45:20 +0300

    fix(blocks): restore reservoir item name contract
        Changes:
        - Port essentia reservoir ItemBlock metadata handling from the original class instead of using the generic metadata item helper
        - Preserve reservoir placement facing behavior while keeping the item display name on the base block translation key
        - Add a static guard for the reservoir item contract

--- src/main/java/thaumcraft/common/blocks/ItemBlocks/BlockEssentiaReservoirItem.java
+++ src/main/java/thaumcraft/common/blocks/ItemBlocks/BlockEssentiaReservoirItem.java
@@ -3,6 +3,7 @@ package thaumcraft.common.blocks.ItemBlocks;
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
@@ -10,9 +11,16 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import thaumcraft.common.tiles.TileEssentiaReservoir;
 
-public class BlockEssentiaReservoirItem extends BlockMetadataItem {
+public class BlockEssentiaReservoirItem extends ItemBlock {
     public BlockEssentiaReservoirItem(Block block) {
         super(block);
+        this.setMaxDamage(0);
+        this.setHasSubtypes(true);
+    }
+
+    @Override
+    public int getMetadata(int damage) {
+        return damage;
     }
 
     @Override
